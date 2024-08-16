236ad971425ac4b683763e1237eb957e9fd12c23
Paul Lavoie <plavoie@gmu.edu>
2024-08-16 19:17:38 -0400

    Plotting dummy data dynamically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import statements
 import streamlit as st
+import plotly.express as px
 
 # Creating title for the web app
 st.title("Weather Forecast For The Next Days")
@@ -13,5 +14,24 @@ option = st.selectbox(label="Select data to view", options=("Temperature",
                      "Sky"))
 # Creating subheader for graph
 st.subheader(f"{option} for the next {days} days in {place}")
-print(place)
-print(days)
+
+# Creating a function to dynamically plot fake data
+def get_data(days):
+    # Creating a dummy date list to store dates 
+    dates = ["2022-10-25","2022-10-26","2022-10-27"]  
+    # Creating a dummy temperatures list to store temperatures 
+    temperatures = [23, 31, 36]
+    # Adjusting temperatures to be dynamic
+    temperatures = [days * i for i in temperatures]
+    # Return dates and temperatures
+    return dates, temperatures
+# Calling get_data to dynamically get data points for line graph and deconstructing
+# the returned tuple into variables d and t
+d,t = get_data(days)
+
+# Creating plotly figure to pass into st.plotly_chart() method to create a graph
+# px.line() creates a line chart
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature in F"})
+
+# Passing the figure object into st.plotly_chart()
+st.plotly_chart(figure)
